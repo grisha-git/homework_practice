8fe6d7c34f1e3ac27bc7ed7bdc5e2170c2ecb6be
grisha-git <donkeevgrisa@gmail.com>
2026-03-05 23:39:15 +0300

    Merge function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
 
+template<class T>
+T* merge(const T* a, size_t sa, const T* b, size_t sb, T* c)
+{
+  size_t sizeA = 0;
+  size_t sizeB = 0;
+  size_t sizeC = 0;
+  while(sizeA < sa && sizeB < sb)
+  {
+    if (a[sizeA] <= b[sizeB])
+    {
+      c[sizeC] = a[sizeA];
+      sizeA++;
+      sizeC++;
+    }
+    else
+    {
+      c[sizeC] = b[sizeB];
+      sizeC++;
+      sizeB++;
+    }
+  }
+  while (sizeA < sa)
+  {
+    c[sizeC] = a[sizeA];
+    sizeA++;
+    sizeC++;
+  }
+  while (sizeB < sb)
+  {
+    c[sizeC] = b[sizeB];
+    sizeC++;
+    sizeB++;
+  }
+  return c;
+}
+
 int main()
 {
-  
+
 }
